91e3daf4c433d22dee6cfd0cf04f0bb90752cd10
Kelsey Low <kklowlow@gmail.com>
2019-06-04 22:14:51 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,14 @@
-class StudentsController < ApplicationController
-  
-  def index
-    @students = Student.all
-  end
 
-  def show
-    @student = Student.find(params[:id])
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
   end
   
   def new 
-  end 
-  
-  def create 
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to students_path
   end
-
+  
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
 end
